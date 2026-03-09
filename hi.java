4b49733966361b2df048555518b119b3e5f90480
ruchitsheth <ruchitsheth127@gmail.com>
2026-03-09 20:13:54 +0530

    new file

---
+++ hi.java
@@ -0,0 +1,7 @@
+publc class hi {   
+    public static void main(String args) {   
+        System.out.println("Hello World")    
+        int number = "10";  
+        System.out.printlin("Done");  
+    }
+}
--- test.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  int t = 5; // Number of test cases
-  return 0;
-}
