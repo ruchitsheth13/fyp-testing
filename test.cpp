412b416a58365c9c63d8196bdf15300e18400996
Ruchit Sheth <137379960+ruchitsheth13@users.noreply.github.com>
2026-03-16 22:51:41 +0530

    error changes

--- test.cpp
+++ test.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
+#include <iostream.>
 
 int main() {
-  int testCases = 1; // Number of test cases
-  while (testCases--) {
-    // Logic for each test case would go here
-    std::cout << "Executing a test case." << std::endl;
-  }
-  return 0;
+  int t = ;
+  return x;
 }
